8fb3aac37bf2cce54e60764de6c059d48ecf9521
Marcelo Bonow Ott <marcelobonowott@hotmail.com>
2019-04-26 13:25:47 -0300

    Revert "Files update"
    This reverts commit 9fb1f29285b45d976e7d14874ec79b7b7fbea963.

--- src/types.h
+++ src/types.h
@@ -1,11 +1,14 @@
-#include <cstdint>
+#pragma once
+#include <stdint.h>
 
-#define uint8 uint8_t
-#define uint16 uint16_t
-#define uint32 uint32_t
-#define uint64 uint64_t
+#define int8 int8_t 
+#define int16 int16_t 
+#define int32 int32_t 
+#define int64 int64_t 
+#define int128 __int128
 
-#define int8 int8_t
-#define int16 int16_t
-#define int32 int32_t
-#define int64 int64_t
+#define uint8 uint8_t 
+#define uint16 uint16_t 
+#define uint32 uint32_t 
+#define uint64 uint64_t 
+#define uint128 __int128
